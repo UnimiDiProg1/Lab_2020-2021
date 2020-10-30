77ec91a914dffac284cf496029f634e01a9fa32b
Marco Casazza <dexterux86@gmail.com>
2020-10-30 11:31:51 +0100

    Fix codice esercizio 11
    Il codice corrispondeva ad un esercizio diverso e più complesso.

--- Laboratorio_3/Cicli_annidati/esercizio_11/quadrato_colonne_alterne_2.go
+++ Laboratorio_3/Cicli_annidati/esercizio_11/quadrato_colonne_alterne_2.go
@@ -6,33 +6,25 @@ import (
 
 func main() {
 
-	var numero int
+	var dimensione int
 	fmt.Print("Inserisci un numero: ")
-	fmt.Scan(&numero)
+	fmt.Scan(&dimensione)
 
-	// stampa n righe di caratteri ** e ++ alternati sia sulla stessa riga che sulla colonna
-	var i, j int
-	for i = 0; i < numero; i++ {
-		for j = 0; j < numero; j++ {
+	// stampa n righe di coppie di caratteri * e + alternati sulla stessa riga
+	var indiceRiga, indiceColonna int
+	for indiceRiga = 0; indiceRiga < dimensione; indiceRiga++ {
+		for indiceColonna = 0; indiceColonna < dimensione; indiceColonna++ {
 			// Un modo per risolvere questo problema è quello di considerare 4 casi distinti:
 			// caso 1 e 2 -> stampa di un *
 			// caso 3 e 4 -> stampa di un +
 			// I casi sono 4 perché il pattern che si ripete è di 4 caratteri (**++)
 
-			// Un modo per farlo è quello di distinguere le righe di indice pari e dispari e fare una stampa
-			// differenziata
-			// Il modo proposto in questo codice è invece un'altro:
-			// dato che quello che cambia tra una riga e l'altra è che i simboli **++ sono spostati di due posizioni,
-			// mantengo una nuova variabile chiamata offset che sarà identica a j se siamo in una riga di indice pari,
-			// mentre sarà j+2 (quindi spostata di due posizioni) se l'indice di riga è dispari
-			var offset int = j
-			if i%2 != 0 {
-				offset += 2
-			}
-
-			if offset%4 <= 1 {
+			// utilizzo il resto della divisione tra l'indice di colonna e 4 perché ho 4 casi
+			if indiceColonna%4 <= 1 {
+				// i primi due casi corrispondono ad un resto uguale a 0 o a 1
 				fmt.Print("* ")
 			} else {
+				// i rimanenti due casi corrispondono ad un resto uguale a 2 o a 3
 				fmt.Print("+ ")
 			}
 		}
